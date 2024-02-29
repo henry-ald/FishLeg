46a13dd4042a09eaf965faee4eb39d7a5f789a2a
henry-ald <henry.aldridge.23@ucl.ac.uk>
2024-02-29 21:55:55 +0000

    separate function for getting predictions

--- src/optim/FishLeg/likelihoods/bernoulli_likelihood_yolo.py
+++ src/optim/FishLeg/likelihoods/bernoulli_likelihood_yolo.py
@@ -23,16 +23,20 @@ class BernoulliLikelihood_yolo(FishLikelihoodBase):
         self.device = device
 
     def nll(self, preds: torch.Tensor, observations: torch.Tensor) -> torch.Tensor:
+        preds = self._get_preds(preds)
+        bce = torch.sum(preds * (1.0 - observations) + torch.nn.Softplus()(-preds))
+        return bce / preds.shape[0]
+
+    def draw(self, preds: torch.Tensor) -> torch.Tensor:
+        preds = self._get_preds(preds)
+        return Bernoulli(logits=preds).sample()
+
+    def _get_preds(self, preds: torch.Tensor) -> torch.Tensor:
         # preds is a list of length 2
         # preds[0] is a 3-D tensor: [batch_size; sum of (no. anchor sizes * no. of grid rows * no. of grid cols ); [x ,y , width, height, class, confidences]]
         # we want the confidences from the third axis of preds[0]. this is of length no. of classes
         # preds[1] provides the same info, except in the form of 3 5-D tensors that indexes preds[0] by grid position
         indices = torch.tensor([i for i in range(5, preds[0].shape[2])]).to(self.device) # 1-D tensor of indices corresponding to confidences in preds[0]
         preds = torch.index_select(preds[0], 2, indices) # get the confidences
-        bce = torch.sum(preds * (1.0 - observations) + torch.nn.Softplus()(-preds))
-        return bce / preds.shape[0]
+        return preds
 
-    def draw(self, preds: torch.Tensor) -> torch.Tensor:
-        indices = torch.tensor([i for i in range(5, preds[0].shape[2])]).to(self.device)
-        preds = torch.index_select(preds[0], 2, indices)
-        return Bernoulli(logits=preds).sample()
