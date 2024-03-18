d01ea7c771bfcd87f24c1893492d21a82d65ddaa
henry-ald <henry.aldridge.23@ucl.ac.uk>
2024-03-18 09:39:35 +0000

    add yolo version dictionary initialisation

--- src/optim/FishLeg/likelihoods/__init__.py
+++ src/optim/FishLeg/likelihoods/__init__.py
@@ -4,3 +4,10 @@ from .gaussian_likelihood import *
 from .softmax_likelihood import *
 from .bernoulli_likelihood_yolo import *
 from .likelihood_yolo import *
+from .yolo import v5, v7, v7u6
+
+YOLO_VERSION = {
+    "v5" : v5,
+    "v7" : v7,
+    "v7u6": v7u6
+}
